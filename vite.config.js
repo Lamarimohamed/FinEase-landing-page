0a824b93be26ba0446caff8670d7ea827c0085f1
Mohamed Lamari <Lamarimohamed92@gmail.com>
2025-06-13 16:26:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/FinEase-landing-page/',
   plugins: [react(), tagger()],
   build: {
     outDir: "build",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') return 'assets/[name]-[hash][extname]';
-          return 'assets/[name]-[hash][extname]';
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
   },
   resolve: {
     alias: {
